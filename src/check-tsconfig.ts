ebd563e1d38bdcafb40b484566edb04e46ab80eb
nozch <freegiafrag@gmail.com>
2025-12-17 07:53:54 +0000

    chore: remove temporary file for checking ts.config and linter

--- src/check-tsconfig.ts
+++
@@ -1,9 +0,0 @@
-// // strict null check が効いている
-// const x: string = null;
-// // noImplicitAny が効いている
-// function f(x) { return x; }
-// // strictPropertyInitialization が効いている
-// class A {
-//   name: string;
-// }
-

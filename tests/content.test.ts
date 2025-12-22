eb504bfc23adf5d37504f92875c6b67da9dcc316
nozch <freegiafrag@gmail.com>
2025-12-22 08:26:42 +0000

    test: adjust tests for schema field deletion and renaming

--- tests/content.test.ts
+++ tests/content.test.ts
@@ -1,33 +1,29 @@
-// @vitest-enviroment node
+// @vitest-environment node
 
 import { describe, it, expect } from "vitest";
 import { blogSchema } from "../src/blog.schema";
-import { z } from "astro:content";
-type BlogInput = z.input<typeof blogSchema>;
-
-// todo: dateをどのタイミングで持つか要検討
-const sampleDate = new Date("2025-01-01T00:00:00+09:00");
-
-const draftPost = {
-	visibility: "draft",
-	title: "draft post",
-	date: sampleDate,
-} satisfies BlogInput;
-
-const validPublicPost = {
-	visibility: "public",
-	// generates uuid v4, might be v7 in produciton env.
-	id: crypto.randomUUID(),
-	slug: "some-slug",
-	title: "public post",
-	date: sampleDate,
-} satisfies BlogInput;
-
+import type { BlogInput } from "../src/blog.schema";
 describe("blogSchema", () => {
+
+	const draftPost = {
+		visibility: "draft",
+		title: "draft post",
+	} satisfies BlogInput;
+	
 	it("draft はid/slug なしでも通る", () => {
 		const result = blogSchema.safeParse(draftPost);
 		expect(result.success).toBe(true);
 	});
+
+	const sampleDate = new Date("2025-01-01T00:00:00+09:00");
+	const validPublicPost = {
+		visibility: "public",
+		// generates uuid v4, might be v7 in produciton env.
+		id: crypto.randomUUID(),
+		slug: "some-slug",
+		title: "public post",
+		publishedAt: sampleDate
+	} satisfies BlogInput;
 
 	it("publicはidありでもslugがないと落ちる", () => {
 		const { slug, ...invalidPublicPost } = validPublicPost;

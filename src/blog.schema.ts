1fbfecc1f5ae3af77e9a3bec618d315bf5953f9c
nozch <freegiafrag@gmail.com>
2025-12-22 08:15:00 +0000

    refactor(blog-schema): remove unnecessary date from draft state, rename date to publishedAt for published states

--- src/blog.schema.ts
+++ src/blog.schema.ts
@@ -1,22 +1,19 @@
 import { z } from "astro:content";
-
 export const blogSchema = z.discriminatedUnion("visibility", [
 	z.object({
 		visibility: z.literal("draft"),
 		title: z.string(),
-		date: z.coerce.date(),
 		description: z.string().optional(),
 		tags: z.array(z.string()).default([]),
 	}),
-
-	// draft以外はid, slugを必須とする.
 	z.object({
 		visibility: z.enum(["public", "private", "unlisted", "withdrawn"]),
 		id: z.string().uuid(),
 		slug: z.string(),
 		title: z.string(),
-		date: z.coerce.date(),
+		publishedAt: z.coerce.date(),
 		description: z.string().optional(),
 		tags: z.array(z.string()).default([]),
 	}),
 ]);
+export type BlogInput = z.input<typeof blogSchema>
